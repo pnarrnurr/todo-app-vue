58e1257be9facf8142d21a981b5bc5964bf5027c
pnarrnurr <pinarsarigul984@gmail.com>
2023-08-09 13:27:08 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,55 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="content mx-5">
+    <h1>Todo App</h1>
+    <form @submit.prevent="addTodo">
+      <div class="field">
+        <div class="control">
+          <input type="text" v-model="todo" class="input" placeholder="Todo Girin">
+        </div>
+      </div>
+      <button type="submit" class="button is-warning">Ekle</button>
+    </form>
+    <div v-for="todo in todos" :key="todo.id" class="card m-5">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left"></div>
+          <div class="media-content">
+            <p class="title cursor" @click="done(todo)" :class="{ done: todo.done }">
+              {{ todo.content }}
+            </p>
+            <p>Yapıldı: {{ todo.done }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const todo = ref('');
+    const todos = ref([]);
+
+    const addTodo = () => {
+      todos.value.push({
+        done: false,
+        content: todo.value,
+        id: Date.now()
+      })
+      todo.value = '';
+    }
+
+    const done = (todo) => {
+      todo.done = !todo.done;
+    }
+
+    return { todo, todos, addTodo, done };
+  }
+}
+
+</script>
 
 <style>
 #app {
@@ -26,5 +71,13 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.done {
+  text-decoration: line-through;
+}
+
+.cursor {
+  cursor: pointer;
 }
 </style>
